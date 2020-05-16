6bf2c62c15ef667d3f88d2cda1a58969aabc7191
Christhian Sandoval <csandovalchauca@hotmail.com>
2020-05-16 17:07:22 -0500

    Upload CMD

---
+++ java-reposclone.cmd
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+TITLE CLONAR REPOSITORIOS JAVA
+
+:: PROYECTOS PERSONALES O DEL TRABAJO
+
+ECHO ==== SISO-SELENIUM ====
+CALL git clone https://github.com/Christhiansch/selenium.git
+ECHO:
+
+ECHO ==== AUTOMATIZATION PROJECT ====
+CALL git clone https://github.com/Josdy/AutomatizationProject.git
+ECHO:
+
+:: PROYECTOS EXTERNOS
+
+ECHO ==== OLX - JUEGO DAMAS ====
+CALL git clone https://github.com/Christhiansch/juegodamas.git
+ECHO:
+
+PAUSE
